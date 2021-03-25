2508af4eaecf505c29e8c7a5fcc60f35dbdb9896
fachrudin <fachrudin.2000@students.amikom.ac.id>
2021-03-25 10:21:15 +0700

    Initial commit

---
+++ app/src/main/java/com/masudinn/kade1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.masudinn.kade1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
